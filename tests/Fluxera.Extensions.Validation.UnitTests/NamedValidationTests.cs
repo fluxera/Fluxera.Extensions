0a9b10ab8140a4a375343ce24df44a4203930863
Matthias Gernand <mgernand@users.noreply.github.com>
2021-12-13 14:59:54 +0100

    Fixed resolving of multiple named services. (#12)

---
+++ tests/Fluxera.Extensions.Validation.UnitTests/NamedValidationTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Fluxera.Extensions.Validation.UnitTests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using FluentAssertions;
+	using Fluxera.Extensions.DependencyInjection;
+	using Fluxera.Extensions.Validation.DataAnnotations;
+	using Fluxera.Extensions.Validation.FluentValidation;
+	using Microsoft.Extensions.DependencyInjection;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class NamedValidationTests
+	{
+
+		[Test]
+		public async Task ShouldGetNamedValidatorFactories()
+		{
+			IServiceCollection services = new ServiceCollection();
+
+			services.AddValidation(builder =>
+			{
+				builder
+					.AddDataAnnotations("Name")
+					.AddFluentValidation("Name", registration =>
+					{
+						registration.AddValidator<PersonValidator>();
+					});
+			});
+
+			IServiceProvider serviceProvider = services.BuildServiceProvider();
+			IEnumerable<IValidatorFactory> validatorFactories = serviceProvider.GetNamedServices<IValidatorFactory>("Name");
+
+			validatorFactories.Count().Should().Be(2);
+		}
+	}
+}

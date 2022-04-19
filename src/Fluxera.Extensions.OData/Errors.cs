8a4cf3a18eb5e0e236992973c767465e8743daec
Matthias Gernand <mgernand@users.noreply.github.com>
2022-04-19 17:23:39 +0200

    Added additional validator type configuration method. (#26)
    * Added some additional features.
    
    * Added additional validation registration method.

---
+++ src/Fluxera.Extensions.OData/Errors.cs
@@ -0,0 +1,27 @@
+﻿namespace Fluxera.Extensions.OData
+{
+	using System;
+	using JetBrains.Annotations;
+
+	[PublicAPI]
+	internal static class Errors
+	{
+		public static Exception CanNotUpdateTransientItem()
+		{
+			const string message = "A transient item can not be updated. Add the item first.";
+			return new InvalidOperationException(message);
+		}
+
+		public static Exception CanNotAddExistingItem()
+		{
+			const string message = "A non-transient item can not be added.";
+			return new InvalidOperationException(message);
+		}
+
+		public static Exception CanNotDeleteTransientItem()
+		{
+			const string message = "A transient item can not be deleted. Add the item first.";
+			return new InvalidOperationException(message);
+		}
+	}
+}
--- src/Fluxera.Extensions.Validation.FluentValidation/ValidatorRegistration.cs
+++ src/Fluxera.Extensions.Validation.FluentValidation/ValidatorRegistration.cs
@@ -22,7 +22,7 @@
 		}
 
 		/// <summary>
-		///		Adds all validators available in the given assemblies.
+		///     Adds all validators available in the given assemblies.
 		/// </summary>
 		/// <param name="assemblies">The assemblies to scan.</param>
 		/// <returns>The registration.</returns>
@@ -30,7 +30,7 @@
 		{
 			assemblies ??= Enumerable.Empty<Assembly>();
 
-			foreach (Assembly assembly in assemblies)
+			foreach(Assembly assembly in assemblies)
 			{
 				this.AddValidators(assembly);
 			}
@@ -39,7 +39,7 @@
 		}
 
 		/// <summary>
-		///		Adds all validators available in the assembly.
+		///     Adds all validators available in the assembly.
 		/// </summary>
 		/// <param name="assembly">The assembly to scan.</param>
 		/// <returns>The registration.</returns>
@@ -49,7 +49,7 @@
 
 			// Register FluentValidation validators.
 			IEnumerable<Type> types = assembly.GetTypes().Where(type => !type.IsInterface && !type.IsAbstract && type.Implements<IValidator>());
-			foreach (Type validatorType in types)
+			foreach(Type validatorType in types)
 			{
 				this.AddValidator(validatorType);
 			}
@@ -58,7 +58,24 @@
 		}
 
 		/// <summary>
-		///		Adds the validator type.
+		///     Adds all validator types.
+		/// </summary>
+		/// <param name="types">The assemblies to scan.</param>
+		/// <returns>The registration.</returns>
+		public ValidatorRegistration AddValidators(IEnumerable<Type> types)
+		{
+			types ??= Enumerable.Empty<Type>();
+
+			foreach(Type type in types)
+			{
+				this.AddValidator(type);
+			}
+
+			return this;
+		}
+
+		/// <summary>
+		///     Adds the validator type.
 		/// </summary>
 		/// <param name="validatorType">The validator type.</param>
 		/// <returns>The registration.</returns>
@@ -66,7 +83,7 @@
 		{
 			Guard.Against.Null(validatorType, nameof(validatorType));
 
-			foreach (Type interfaceType in validatorType.GetInterfaces().Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IValidator<>)))
+			foreach(Type interfaceType in validatorType.GetInterfaces().Where(x => x.IsGenericType && (x.GetGenericTypeDefinition() == typeof(IValidator<>))))
 			{
 				this.services.TryAddTransient(interfaceType, validatorType);
 				this.services.TryAddTransient(validatorType);
@@ -76,7 +93,7 @@
 		}
 
 		/// <summary>
-		///		Adds the validator type.
+		///     Adds the validator type.
 		/// </summary>
 		/// <typeparam name="TValidator">The validator type.</typeparam>
 		/// <returns>The registration.</returns>

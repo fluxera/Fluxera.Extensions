8b61383a71a1a5472e2568b069add1bbc83861da
Matthias Gernand <matthias.gernand@fluxera.com>
2022-06-03 23:26:32 +0200

    Implemented the validation exception class.

--- src/Fluxera.Extensions.Validation/ValidationException.cs
+++ src/Fluxera.Extensions.Validation/ValidationException.cs
@@ -2,29 +2,28 @@
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Runtime.Serialization;
+	using Fluxera.Guards;
 	using JetBrains.Annotations;
 
+	/// <summary>
+	///     An exception that provides error messages.
+	/// </summary>
 	[PublicAPI]
 	[Serializable]
 	public sealed class ValidationException : Exception
 	{
-		public ValidationException()
+		/// <summary>
+		///     Initializes a new instance of the <see cref="ValidationException" /> type.
+		/// </summary>
+		/// <param name="errors"></param>
+		public ValidationException(ICollection<ValidationError> errors)
 		{
+			this.Errors = Guard.Against.Null(errors);
 		}
 
-		public ValidationException(string message) : base(message)
-		{
-		}
-
-		public ValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
-		{
-		}
-
-		public ValidationException(string message, Exception innerException) : base(message, innerException)
-		{
-		}
-
-		public ICollection<ValidationError> Errors { get; } = new List<ValidationError>();
+		/// <summary>
+		///     Gets the error messages.
+		/// </summary>
+		public ICollection<ValidationError> Errors { get; }
 	}
 }

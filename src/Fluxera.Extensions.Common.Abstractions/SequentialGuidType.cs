0a3615272e7eaadff9c62d03de53be203464d759
Matthias Gernand <matthias.gernand@fluxera.com>
2023-11-15 17:45:43 +0100

    Upgraded to .NET 8

--- src/Fluxera.Extensions.Common.Abstractions/SequentialGuidType.cs
+++ src/Fluxera.Extensions.Common.Abstractions/SequentialGuidType.cs
@@ -14,7 +14,7 @@
 		SequentialAsString,
 
 		/// <summary>
-		///     The GUID should be sequential when formatted using the <see cref="Guid.ToByteArray" /> method.
+		///     The GUID should be sequential when formatted using the <see cref="Guid.ToByteArray()" /> method.
 		///     Used by Oracle.
 		/// </summary>
 		SequentialAsBinary,
